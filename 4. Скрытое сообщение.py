083111d575a995ad8aa2d9dd967ff0a12933919c
andrey.isaev <an.isaev7@gmail.com>
2022-07-15 21:25:50 +0300

    ready

---
+++ 4. Скрытое сообщение.py
@@ -0,0 +1,10 @@
+message = 'Мн3 4у9ится, 4т0 кт0-т0 4итает с006щения!!! М0ж3т я 8се при9умала, н0 6у9ь на43ку, 0к?'
+
+message = message.replace("3", "Е".lower())
+message = message.replace("4", "Ч".lower())
+message = message.replace("6", "Б".lower())
+message = message.replace("8", "В".lower())
+message = message.replace("9", "Д".lower())
+message = message.replace("0", "О".lower())
+
+print(message)
