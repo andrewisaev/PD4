7e267c5ae8d0e5eaca5359898dd7f07260a21302
andrey.isaev <an.isaev7@gmail.com>
2022-07-15 21:59:27 +0300

    ready +/-

--- 5. Почта на корпоративном домене.py
+++ 5. Почта на корпоративном домене.py
@@ -1,10 +1,11 @@
 user_email = input()
 free_domain = ["yandex.ru", "mail.ru", "gmail.com", "yahoo.com", "rambler.ru"]
 
+user_domain = user_email.split("@")[1]
 
-if "." not in user_email.split("@")[1]:
+if "@" not in user_email or "." not in user_domain:
     print("Это вообще не почта")
-elif user_email.split("@")[1] in free_domain:
+elif user_domain in free_domain:
     print("Это почта, она на бесплатном домене")
 else:
     print("Это почта, она на корпоративном домене")
